2b44f4b7e899ce91044c9462921b532e191d0a95
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-11-29 17:14:29 +0100

    changed bradius size on search input

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ breed, size, hair, img, wiki }) => {
   return (
-    <div className="tc grow br3 pa3 ma2 dib bw2 shadow-5 img-wrapper">
+    <div className="tc grow br1 pa3 ma2 dib bw2 shadow-5 img-wrapper">
       <a href={`${wiki}`} target="_blank" rel="noopener noreferrer">
         <img alt="cats" src={`${img}`} />
         <div>
