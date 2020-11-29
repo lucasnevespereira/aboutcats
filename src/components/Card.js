bc315a08370357598746c32840086e3c7c57d642
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-11-29 17:08:45 +0100

    final touches

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({ breed, size, hair, img, wiki }) => {
   return (
     <div className="tc grow br3 pa3 ma2 dib bw2 shadow-5 img-wrapper">
-      <a href={`${wiki}`} target="_blank">
+      <a href={`${wiki}`} target="_blank" rel="noopener noreferrer">
         <img alt="cats" src={`${img}`} />
         <div>
           <h2>{breed}</h2>
